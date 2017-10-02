22b824d62ef7a09288ef7696d012acf6cd5ea7a4
moranpatrick <patrickmoran121@gmail.com>
2017-10-02 22:18:07 +0100

    Completed Part 6

--- 06-largest_smallest.py
+++ 06-largest_smallest.py
@@ -7,12 +7,23 @@ import random
 
 random_list = [] # Array to hold the list of numbers
 
-# Sort List using min and max functions
+# Get the biggest and smallest numbers in the list using min and max functions
 def sort(list):
     biggest = max(list)
     smallest = min(list)
-
     print("The Largest Number in the list is: %d.\nThe Smallest number in the list is: %d." % (biggest, smallest))
+
+# Get the biggest and smallest numbers in the list using a loop
+def sortWithLoop(list):
+    max = 0
+    min = 100
+    for i in list:
+        if i > max:
+            max = i
+        if i < min:
+            min = i
+    print("The largest number in the list is %d" % max)
+    print("The smallest number in the list is %d" % min)
 
 # Create a list of 10 random numbers from 1 to 100
 for i in range (10):
@@ -20,9 +31,10 @@ for i in range (10):
 
 # Print off the list
 print (random_list)
+
+# Call both functions passing in the random list 
 sort(random_list)
-
-
+sortWithLoop(random_list)
 
 # References
 # https://stackoverflow.com/questions/16655089/python-random-numbers-into-a-list
